053cb1dd63e8d30298227827b6bd770bac41066d
Melvin Lammerts <melvinrts@gmail.com>
2023-03-22 16:06:35 +0100

    Add IPs switch

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"regexp"
@@ -10,12 +12,16 @@ import (
 )
 
 func main() {
-	// get the ASN from the first command-line argument
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: go run main.go <ASN>")
+	var ips bool
+	flag.BoolVar(&ips, "ips", false, "Print IPs instead of ASN")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Fprintln(os.Stderr, "Usage: go run main.go [-ips] <ASN>")
 		return
 	}
-	asn := os.Args[1]
+
+	asn := flag.Args()[0]
 
 	// normalize to correct format
 	asn = strings.ToLower(asn)
@@ -45,8 +51,45 @@ func main() {
 	re := regexp.MustCompile(`href="/demo/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})</a>`)
 	subnets := re.FindAllStringSubmatch(string(body), -1)
 
-	// print the subnets
+	if ips {
+		printIps(subnets)
+	} else {
+		printSubnets(subnets)
+	}
+}
+
+func printIps(subnets [][]string) {
+	for _, subnet := range subnets {
+		ips, _ := getIPAddressesInSubnet(subnet[2])
+		for _, ip := range ips {
+			fmt.Println(ip)
+		}
+	}
+}
+
+func printSubnets(subnets [][]string) {
 	for _, subnet := range subnets {
 		fmt.Println(subnet[2])
+	}
+}
+
+func getIPAddressesInSubnet(subnet string) ([]string, error) {
+	ip, ipNet, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return nil, err
+	}
+	var ips []string
+	for ip := ip.Mask(ipNet.Mask); ipNet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	return ips, nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
 	}
 }
